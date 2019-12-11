563457308ec044826a603ce1d395c19d4186276a
GuanchengFu <sggfu.rick@gmail.com>
2019-12-11 22:07:22 +0000

    Finish q18

--- projects/scheme/questions.scm
+++ projects/scheme/questions.scm
@@ -6,7 +6,10 @@
 ; Some utility functions that you may find useful to implement.
 
 (define (cons-all first rests)
-  'replace-this-line)
+    (map (lambda (s) (append (list first) s))  rests)
+)
+
+
 
 (define (zip pairs)
   'replace-this-line)
@@ -15,16 +18,36 @@
 ;; Returns a list of two-element lists
 (define (enumerate s)
   ; BEGIN PROBLEM 17
-  'replace-this-line
+  (define (enum s k)
+      (if (null? s)
+          nil
+          (begin (define k (+ k 1))
+                 (cons (cons k (cons (car s) nil)) (enum (cdr s) k))))
+  )
+  (enum s (- 1))
   )
   ; END PROBLEM 17
 
 ;; Problem 18
 ;; List all ways to make change for TOTAL with DENOMS
+;; cons-all will not evaluate to the right value if the rests is nil
 (define (list-change total denoms)
+        ;;; If this condition is reached, the related element will be nil.
+        ;;; because (cons-all item nil) = nil.
+        ;;; This condtion will remove all the bad conditions.
+        ;;; For instance (list-change 10 '(5 2)) will not have the (5 2 2 2)
+        (cond ((or (null? denoms) (<= total 0)) nil)
+              ((< total (car denoms)) (list-change total (cdr denoms)))
+              ;;; This is the base case.  The last step of any list-change
+              ;;; is when the total equals to one of the elements of denoms.
+              ((equal? total (car denoms)) (append (list (list total))
+                                                   (list-change total (cdr denoms))))
+              (else (append (cons-all (car denoms) (list-change (- total (car denoms)) denoms))
+                            (list-change total (cdr denoms)))))
+        )
   ; BEGIN PROBLEM 18
-  'replace-this-line
-  )
+
+
   ; END PROBLEM 18
 
 ;; Problem 19
